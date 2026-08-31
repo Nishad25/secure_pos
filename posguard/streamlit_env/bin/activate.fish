873715b7ac96cbe3c3aec2ee730360edd8839081
Nishad25 <mohnishad5432@gmail.com>
2026-08-31 00:23:17 +0000

    updated codes with sdn

--- posguard/streamlit_env/bin/activate.fish
+++ posguard/streamlit_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/nishad/securePos/posguard/streamlit_env
+set -gx VIRTUAL_ENV /home/nishad/secure_pos/posguard/streamlit_env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
